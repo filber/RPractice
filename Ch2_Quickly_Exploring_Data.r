aa33b6c233f73dbece779d64e3361e581b1496a7
filber <filber@foxmail.com>
2016-01-25 11:44:32 +0800

    第二章的示例.

--- Ch2_Quickly_Exploring_Data.r
+++ Ch2_Quickly_Exploring_Data.r
@@ -3,9 +3,9 @@ library(gcookbook)
 
 
 #2.1. Scatter Plot
-#使用graphics中的plot绘制scatter plot
+#x和y均为continuous的vector
 plot(mtcars$wt,mtcars$mpg)
-
+#equals to 
 qplot(mtcars$wt,mtcars$mpg)
 #equals to 
 qplot(x = wt,y = mpg,data = mtcars)
@@ -15,7 +15,7 @@ qplot(x = wt,y = mpg,data = mtcars)
 ggplot(data = mtcars,mapping = aes(x = wt,y = mpg))+geom_point()
 
 #2.2. Line Graph
-#先画线,再画点
+#先画线,再画点,points函数具备追加功能.
 plot(pressure$temperature,pressure$pressure,type='l')
 points(pressure$temperature,pressure$pressure)
 
@@ -47,3 +47,29 @@ ggplot(mtcars,aes(x=mpg))+geom_histogram(binwidth=4)
 
 
 #2.5. Box Plot
+#x是factor,则会默认绘制Box Plot
+plot(x=ToothGrowth$supp,y=ToothGrowth$len)
+#equals to
+boxplot(len~supp,data = ToothGrowth)
+#equals to
+qplot(ToothGrowth$supp,ToothGrowth$len,geom='boxplot')
+
+#x轴用suppo和dose交叉组合
+boxplot(len~supp+dose,data = ToothGrowth)
+qplot(interaction(ToothGrowth$supp,ToothGrowth$dose),ToothGrowth$len,geom='boxplot')
+ggplot(ToothGrowth,aes(x = interaction(supp,dose),y = len))+geom_boxplot()
+
+
+#2.6. Function Curve
+curve(expr = x^3-5*x,from = -10,to = 10)
+myfun <- function(xvar) {
+  1/(1 +exp(-xvar + 10))
+}
+#expr接受一个以numeric vector做输入,同时输出一个numeric vector的函数.
+curve(expr = myfun(x),from=0,to=20)
+#使用add=TRUE表明在现有图形上追加绘制.
+curve(1-myfun(x),add = TRUE,col = "red")
+
+qplot(x = c(0,20),fun=myfun,stat='function',geom = 'line')
+#equals to
+ggplot(data.frame(x=c(0, 20)),aes(x=x)) +stat_function(fun=myfun,geom="line")
