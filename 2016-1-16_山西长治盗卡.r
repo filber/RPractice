14176ff149a72286ed61da72e96d972a8a99ab2f
filber <filber@foxmail.com>
2016-01-25 19:45:44 +0800

    修正了盗卡统计R脚本的名称.

--- 2016-1-16_山西长治盗卡.r
+++ 2016-1-16_山西长治盗卡.r
@@ -16,6 +16,7 @@ findResult <- con$aggregate(pipeline='[
                 "rcgphone" : "$order.body.rcg_phone",
                 "payamt" : "$order.body.pay_amt",
                 "orderrsp" : "$order.body.rspcode",
+                "orderriskresult" : "$order.body.riskresultmsg",
                 "noticersp" : "$notice.body.rspcode", 
                 "paytranresult" : "$notice.body.pay_tran_result"
               }},
@@ -24,20 +25,23 @@ findResult <- con$aggregate(pipeline='[
 
 #Data Preparation
 findResult$payamt=as.integer(findResult$payamt)/100
-findResult$paytranresult[is.na(findResult$paytranresult)]="NONE"
+findResult$paytranresult[is.na(findResult$paytranresult)]=findResult$orderriskresult[is.na(findResult$paytranresult)]
 
-View(findResult)
+#View(findResult)
 
 #Rough Draw,detect there are two segments
 #ggplot(findResult,aes(x = created,y = payamt))+geom_line()
 
-ggplot(findResult,aes(x = factor(created),y = payamt,group=1)) +
+ggplot(findResult,aes(x = factor(substring(created,6,19)),y = payamt,group=1)) +
   geom_point(size=8,aes(shape=paytranresult)) +
   geom_line() +
-  ylim(0,max(findResult$payamt)+100) +
+  ylim(-200,max(findResult$payamt)+800) +
   geom_vline(xintercept=8.5,linetype="dashed") +
-  annotate("rect",ymin = 0,ymax = max(findResult$payamt),xmin = 11.5,xmax = 17.5,fill="blue",alpha=.1) +
+  annotate("rect",ymin = -150,ymax = 750,xmin = 11.5,xmax = 17.5,fill="blue",alpha=.1) +
   geom_text(aes(label=bankcardmask,vjust=-2),colour="red") +
   geom_text(aes(label=substr(orderphone,8,11),vjust=-4)) +
   geom_text(aes(label=substr(rcgphone,8,11),vjust=2)) +
-  ylab("金额") + xlab("时间")
+  geom_text(aes(label=paste("¥",payamt,sep=""),vjust=-6),colour="blue") + #金额
+  ylab("金额") + xlab("时间") +
+  coord_fixed(ratio = 1/500)
+  
