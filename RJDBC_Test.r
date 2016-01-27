4341e97bfa5e7c16bb020204d24b57580ecc8741
filber <filber@foxmail.com>
2016-01-27 20:27:32 +0800

    测试Oracle JDBC的R脚本.

---
+++ RJDBC_Test.r
@@ -0,0 +1,11 @@
+library(RJDBC)
+
+#JDBC Driver
+drv<-JDBC(driverClass = "oracle.jdbc.driver.OracleDriver",classPath = "C:/Users/Administrator/.m2/repository/com/oracle/ojdbc14/10.2.0.1.0/ojdbc14-10.2.0.1.0.jar",identifier.quote = "")
+#JDBC Connection
+con<-dbConnect(drv,"jdbc:oracle:thin:showoperate/sh0wop1rAT@192.168.11.141:1521:ora")
+
+result<-dbGetQuery(con,"select * from t_pay_info where rownum < 10")
+
+
+dbDisconnect(con)
